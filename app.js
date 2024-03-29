80c6ff8847f41017bb436769229227fd46afa220
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-03-29 13:30:01 +0100

    html v2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use srict';
 
+// used for navigation to h1 elments on the page
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
